3f2e6d93a5b54aad49de3907b8cbbf1347c2b37b
Wei-Hao Wang <wwang03@risd.edu>
2019-03-04 23:06:11 -0500

    w
    w

--- scripts/output_Display.js
+++ scripts/output_Display.js
@@ -1,18 +1,3 @@
-// var thisKeys = Object.keys(c);
-// var thisVals = Object.values(c);
-// var thisLength = thisKeys.length;
-// console.log(thisVals);
-// for(i = 0;i<thisLength;i++){
-// 	if(i%10==0){
-// 		$("#container").append("<div class = 'c ids rows'></div><div class = 'c vals rows'></div>");
-// 	}
-// 	$(".ids").last().append("<div><h1>"+thisKeys[i]+"</h1></div>");
-// 	$(".vals").last().append("<div><h2>"+thisVals[i]+"</h2></div>");
-// }
-// var thisSup = 10-(thisLength%10);
-// $(".ids").last().append("<div id = 'thisSup' style = 'flex:"+thisSup+"'></div>");
-// $(".vals").last().append("<div id = 'thisSup' style = 'flex:"+thisSup+"'></div>");
-
 display(Uc);
 display(Lc);
 display(G);
